a8768d3d41dabf07a6ea4197f0d425cf2a2a408f
solygambas <solygambas@gmail.com>
2020-06-16 12:37:39 +0200

    add scalar types and custom types

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,29 +1,104 @@
 import { GraphQLServer } from "graphql-yoga";
 
+// Scalar types - String, Boolean, Int, Float, ID
+
 // type definitions
+// const typeDefs = `
+// type Query {
+//     id: ID!
+//     name: String!
+//     age: Int!
+//     employed: Boolean!
+//     gpa: Float
+// }
+// `;
+// const typeDefs = `
+// type Query {
+//     title: String!
+//     price: Float!
+//     releaseYear: Int
+//     rating: Float
+//     inStock: Boolean!
+// }
+// `;
+
+// resolvers
+// const resolvers = {
+//   Query: {
+//     id() {
+//       return "abc123";
+//     },
+//     name() {
+//       return "Andrew";
+//     },
+//     age() {
+//       return 27;
+//     },
+//     employed() {
+//       return true;
+//     },
+//     gpa() {
+//       return null;
+//     },
+//   },
+// };
+// const resolvers = {
+//   Query: {
+//     title() {
+//       return "Dune";
+//     },
+//     price() {
+//       return 19.99;
+//     },
+//     releaseYear() {
+//       return 1965;
+//     },
+//     rating() {
+//       return 4.5;
+//     },
+//     inStock() {
+//       return true;
+//     },
+//   },
+// };
+
 const typeDefs = `
 type Query {
-    hello: String!
-    name: String!
-    location: String!
-    bio: String!
+    me: User!
+    post: Post!
+}
+type User {
+  id: ID!
+  name: String!
+  email: String!
+  age: Int
+ }
+
+type Post {
+  id: ID!
+  title: String!
+  body: String!
+  isPublished: Boolean!
 }
 `;
 
-// resolvers
 const resolvers = {
   Query: {
-    hello() {
-      return "This is my first query!";
+    me() {
+      return {
+        id: "123",
+        name: "Mike",
+        email: "mike@example.com",
+        age: 28,
+      };
     },
-    name() {
-      return "Andrew";
-    },
-    location() {
-      return "Philadelphia";
-    },
-    bio() {
-      return "I live in Philly and teach on Udemy!";
+    post() {
+      return {
+        id: "123",
+        title: "GraphQL 101",
+        body: "",
+        isPublished: false,
+      };
     },
   },
 };
