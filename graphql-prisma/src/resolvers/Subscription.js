6225ebc61a1dd90cadc3f1d18fdbfe0a8a95d715
solygambas <solygambas@gmail.com>
2020-06-23 09:39:24 +0200

    add prisma mutations and subscriptions

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -1,18 +1,18 @@
 const Subscription = {
   comment: {
-    subscribe(parent, { postId }, { db, pubsub }, info) {
-      const post = db.posts.find(
-        (post) => post.id === postId && post.isPublished
+    subscribe(parent, { postId }, { prisma }, info) {
+      return prisma.subscription.comment(
+        { where: { node: { post: { id: postId } } } },
+        info
       );
-      if (!post) {
-        throw new Error("Post not found.");
-      }
-      return pubsub.asyncIterator(`comment ${postId}`);
     },
   },
   post: {
-    subscribe(parent, args, { pubsub }, info) {
-      return pubsub.asyncIterator("post");
+    subscribe(parent, args, { prisma }, info) {
+      return prisma.subscription.post(
+        { where: { node: { isPublished: true } } },
+        info
+      );
     },
   },
 };
